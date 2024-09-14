191f97c549383ea250d3e4de2825862b5994f8d7
cliffordgail01 <gailcliff100@gmail.com>
2024-09-13 20:29:27 -0400

    project 2

---
+++ csf_assign02/csf_assign02/c_imgproc_fns.c
@@ -0,0 +1,75 @@
+// C implementations of image processing functions
+
+#include <stdlib.h>
+#include <assert.h>
+#include "imgproc.h"
+
+// TODO: define your helper functions here
+
+// Mirror input image horizontally.
+// This transformation always succeeds.
+//
+// Parameters:
+//   input_img  - pointer to the input Image
+//   output_img - pointer to the output Image (in which the transformed
+//                pixels should be stored)
+void imgproc_mirror_h( struct Image *input_img, struct Image *output_img ) {
+  // TODO: implement
+}
+
+// Mirror input image vertically.
+// This transformation always succeeds.
+//
+// Parameters:
+//   input_img  - pointer to the input Image
+//   output_img - pointer to the output Image (in which the transformed
+//                pixels should be stored)
+void imgproc_mirror_v( struct Image *input_img, struct Image *output_img ) {
+  // TODO: implement
+}
+
+// Transform image by generating a grid of n x n smaller tiles created by
+// sampling every n'th pixel from the original image.
+//
+// Parameters:
+//   input_img  - pointer to original struct Image
+//   n          - tiling factor (how many rows and columns of tiles to generate)
+//   output_img - pointer to the output Image (in which the transformed
+//                pixels should be stored)
+// Returns:
+//   1 if successful, or 0 if either
+//     - n is less than 1, or
+//     - the output can't be generated because at least one tile would
+//       be empty (i.e., have 0 width or height)
+int imgproc_tile( struct Image *input_img, int n, struct Image *output_img ) {
+  // TODO: implement
+  return 0;
+}
+
+// Convert input pixels to grayscale.
+// This transformation always succeeds.
+//
+// Parameters:
+//   input_img  - pointer to the input Image
+//   output_img - pointer to the output Image (in which the transformed
+//                pixels should be stored)
+void imgproc_grayscale( struct Image *input_img, struct Image *output_img ) {
+  // TODO: implement
+}
+
+// Overlay a foreground image on a background image, using each foreground
+// pixel's alpha value to determine its degree of opacity in order to blend
+// it with the corresponding background pixel.
+//
+// Parameters:
+//   base_img - pointer to base (background) image
+//   overlay_img - pointer to overlaid (foreground) image
+//   output_img - pointer to output Image
+//
+// Returns:
+//   1 if successful, or 0 if the transformation fails because the base
+//   and overlay image do not have the same dimensions
+int imgproc_composite( struct Image *base_img, struct Image *overlay_img, struct Image *output_img ) {
+  // TODO: implement
+  return 0;
+}
